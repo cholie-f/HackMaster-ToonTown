ededab2e119f42191175c3efd1d86965f2443779
Cholie <choliefighter@gmail.com>
2025-07-10 12:04:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,3 +11,25 @@ if __name__ == "__main__":
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.run_polling()
+
+from telegram.ext import ApplicationBuilder
+from bot.handlers import setup_handlers
+import os
+
+async def post_init(app):
+    await app.bot.set_my_commands([
+        ("start", "Начать взлом сейфа"),
+        ("hack", "Попробовать подобрать код")
+    ])
+
+def main():
+    app = ApplicationBuilder() \
+        .token(os.getenv("TELEGRAM_TOKEN")) \
+        .post_init(post_init) \
+        .build()
+    
+    setup_handlers(app)
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
